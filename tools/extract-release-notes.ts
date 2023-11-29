5363715ffa41ff9d36a2a8b8c8ac238aa215c8c0
Alexander Schumacher <git@story75.com>
2023-11-29 23:03:03 +0100

    chore(ci): fix hooks

--- tools/extract-release-notes.ts
+++ tools/extract-release-notes.ts
@@ -1,9 +1,9 @@
-import {join} from 'node:path';
+import { join } from 'node:path';
 
 const [_bun, _file, version] = Bun.argv;
 
 if (!version) {
-    throw new Error('Version is required');
+  throw new Error('Version is required');
 }
 
 const file = Bun.file(join(import.meta.dir, '../RELEASE_NOTES.md'));
@@ -11,11 +11,14 @@ const content = await file.text();
 
 const fromPosition = content.indexOf(`## ${version}`);
 if (fromPosition === -1) {
-    throw new Error(`Version ${version} not found in RELEASE_NOTES.md`);
+  throw new Error(`Version ${version} not found in RELEASE_NOTES.md`);
 }
 
 const toPosition = content.indexOf(`## `, fromPosition + 1);
-const releaseNotes = content.substring(fromPosition, toPosition === -1 ? undefined : toPosition);
+const releaseNotes = content.substring(
+  fromPosition,
+  toPosition === -1 ? undefined : toPosition,
+);
 
 // eslint-disable-next-line no-console
 console.log(releaseNotes);

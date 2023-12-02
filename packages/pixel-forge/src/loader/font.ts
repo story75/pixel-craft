eb5bf8a3bd6c4af4ee8aeb2de00eac41e1d94d81
Alexander Schumacher <git@story75.com>
2023-12-02 23:24:29 +0100

    feat(loader): add font loader

---
+++ packages/pixel-forge/src/loader/font.ts
@@ -0,0 +1,34 @@
+/**
+ * Load a font by name and path.
+ *
+ * @remarks
+ * The loader will cache fonts to avoid loading the same font multiple times.
+ * The cache key is the name.
+ *
+ * This will automatically add the font to the document once the font is loaded.
+ *
+ * @param name - The name to load the font as.
+ * @param path - The path to load the font from. Can either be relative from the host or a full url.
+ */
+export type FontLoader = (name: string, path: string) => Promise<FontFace>;
+
+/**
+ * Create a font loader.
+ */
+export function createTextureLoader(): FontLoader {
+  const cache = new Map<string, FontFace>();
+
+  return async (name, path) => {
+    const cached = cache.get(name);
+    if (cached) {
+      return cached;
+    }
+
+    const font = new FontFace(name, `url(${path})`);
+    await font.load();
+    document.fonts.add(font);
+
+    cache.set(name, font);
+    return font;
+  };
+}

544a2c847f4333d6034121fff9b6371636202b7f
Alexander Schumacher <git@story75.com>
2024-04-01 23:13:16 +0200

    chore: update bun

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,8 +1,4 @@
-// setting bun-types will break lib option in tsconfig.json
-// see: https://bun.sh/docs/typescript#dom-types
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-
+/// <reference types="@webgpu/types" />
 export {
   inputControlledCamera,
   type InputControlledCamera,
